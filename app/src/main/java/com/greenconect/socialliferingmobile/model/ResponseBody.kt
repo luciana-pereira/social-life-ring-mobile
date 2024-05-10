86d619e6961266c4a8b686a3b2a738a84f4f76fd
l.dos.santos.souza <luci.lv14@gmail.com>
2024-05-10 09:12:21 -0300

    create screen register #2

---
+++ app/src/main/java/com/greenconect/socialliferingmobile/model/ResponseBody.kt
@@ -0,0 +1,10 @@
+package com.greenconect.socialliferingmobile.model
+
+data class ResponseBody(
+    val avatar_url: String,
+    val bio: String?,
+    val email: String?,
+    val full_name: String?,
+    val user_id: Number,
+    val username: String?
+)
---
+++ app/src/main/java/com/greenconect/socialliferingmobile/ui/theme/RegisterActivity.kt
@@ -0,0 +1,15 @@
+package com.greenconect.socialliferingmobile.ui.theme
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.greenconect.socialliferingmobile.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
